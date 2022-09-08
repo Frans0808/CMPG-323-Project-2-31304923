9e0be6a4280053d170cf12a2910aa4af61b007da
Student <Student@nwu.ac.za>
2022-09-08 15:30:06 +0200

    Add project files.

---
+++ IOTDeviceManagement/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOTDeviceManagement.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Device = new HashSet<Device>();
+        }
+
+        [DisplayName("Category ID")]
+        public Guid CategoryId { get; set; }
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
+        [DisplayName("Category Description")]
+        public string CategoryDescription { get; set; }
+        [DisplayName("Category Date Created")]
+        public DateTime DateCreated { get; set; }
+
+        [DisplayName("Device")]
+        public virtual ICollection<Device> Device { get; set; }
+    }
+}
---
+++ IOTDeviceManagement/Models/Device.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace IOTDeviceManagement.Models
+{
+    public partial class Device
+    {
+        [DisplayName("Device ID")]
+        public Guid DeviceId { get; set; }
+        [DisplayName("Device Name")]
+        public string DeviceName { get; set; }
+        [DisplayName("Category ID")]
+        public Guid CategoryId { get; set; }
+        [DisplayName("Zone ID")]
+        public Guid ZoneId { get; set; }
+        [DisplayName("Status")]
+        public string Status { get; set; }
+        [DisplayName("Is Active")]
+        public bool IsActive { get; set; }
+        [DisplayName("Date Created")]
+        public DateTime DateCreated { get; set; }
+
+        [DisplayName("Category")]
+        public virtual Category Category { get; set; }
+        [DisplayName("Zone")]
+        public virtual System.Security.Policy.Zone Zone { get; set; }
+    }
+}
